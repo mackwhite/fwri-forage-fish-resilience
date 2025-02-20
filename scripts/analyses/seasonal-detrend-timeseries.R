ea6104745c7a0ed674e627c0814fe46853d584b1
mackwhite <mwhite@fiu.edu>
2025-02-20 17:06:25 -0500

    seasonally-detrended 21.3m seining time series data.. not quite the data there for haul seines

---
+++ scripts/analyses/seasonal-detrend-timeseries.R
@@ -0,0 +1,125 @@
+###project: Forage Fish Resilience
+###author(s): MW
+###goal(s): seasonal detrend of time series
+###date(s): February 2025
+###note(s): 
+
+# Housekeeping ------------------------------------------------------------
+
+### load necessary libraries ---
+# install.packages("librarian")
+librarian::shelf(tidyverse, tsibble, zoo, forecast, FSA, readr, forcats)
+
+### set simple workflow functions ---
+nacheck <- function(df) {
+      na_count_per_column <- sapply(df, function(x) sum(is.na(x)))
+      print(na_count_per_column)
+}
+
+df <- read_csv("local-data/key-datasets/discrete-community-timeseries.csv") 
+
+### separate by gear and summarize monthly by zone ----
+
+small_seine_zone_df <- df |> 
+      filter(gear_details == "21.3-m Seine") |> 
+      group_by(year, month, estuary, zone) |> 
+      summarize(bm_m2 = mean(total_biomass_m2)) |> 
+      ungroup() |> 
+      mutate(date = make_date(year = year, month = month, day = 1)) |> 
+      mutate(est_zone = paste(estuary, zone, sep = "_"))
+
+big_seine_zone_df <- df |> 
+      filter(gear_details == "Haul Seine") |> 
+      group_by(year, month, estuary, zone) |> 
+      summarize(bm_m2 = mean(total_biomass_m2)) |> 
+      ungroup() |> 
+      mutate(date = make_date(year = year, month = month, day = 1)) |> 
+      mutate(est_zone = paste(estuary, zone, sep = "_"))
+
+### generate function to pull out trend for each estuary-zone combination ----
+
+detrend <- function(df) {
+      # arrange everything by data
+      df <- df |> 
+            arrange(date)
+      # interpolate missing values
+      df$bm_m2 <- na.approx(df$bm_m2, na.rm = FALSE)
+      # check if the group has enough data (at least 24 months)
+      if (nrow(df) < 24) {
+            df$detrended_bm_m2 <- df$bm_m2  # If too few points, return original values
+            return(df)
+      }
+      # convert to tsibble for irregular time series handling
+      df_tsibble <- df |> 
+            as_tsibble(index = date)
+      # convert to time series format
+      tsdf <- ts(df_tsibble$bm_m2, frequency = 12, start = c(min(df$year), min(df$month)))
+      # fit stl to determine seasonality - 'periodic' means fixed seasonality
+      fit <- stl(tsdf, s.window = "periodic")
+      # extract detrended time series
+      df$detrended_bm_m2 <- as.numeric(fit$time.series[,"trend"])
+      # return dataframe
+      return(df)
+}
+
+small_seine_detrended <- small_seine_zone_df |> 
+      group_by(estuary, zone) |> 
+      group_split() |> 
+      lapply(detrend) |> 
+      bind_rows()
+
+### checking out how seasonality was handled ---
+small_seine_detrended |> 
+      filter(estuary == "Apalachicola Bay", zone == "A") |> 
+      ggplot(aes(x=date)) +
+      geom_line(aes(y = bm_m2, color = "Original bm_m2"), size = 1) +
+      geom_line(aes(y = detrended_bm_m2, color = "Detrended bm_m2"), size = 1, linetype = "dashed") +
+      labs(title = "Comparison of Original and Detrended Biomass",
+           x = "Date",
+           y = "Biomass (bm_m2)",
+           color = "Legend") +
+      theme_minimal()
+
+### generate function to visualize sesonality for each estuary-zone combination ----
+
+seasonality <- function(df) {
+      # arrange everything by data
+      df <- df |> 
+            arrange(date)
+      # interpolate missing values
+      # df$bm_m2 <- na.approx(df$bm_m2, na.rm = FALSE)
+      # check if the group has enough data (at least 24 months)
+      if (nrow(df) < 24) {
+            df$seasonal_bm_m2 <- NA  # If too few points, return original values
+            return(df)
+      }
+      # convert to tsibble for irregular time series handling
+      # df_tsibble <- df |> 
+      #       as_tsibble(index = date)
+      # convert to time series format
+      tsdf <- ts(df$bm_m2, frequency = 12, start = c(min(df$year), min(df$month)))
+      # fit stl to determine seasonality - 'periodic' means fixed seasonality
+      fit <- stl(tsdf, s.window = "periodic")
+      # extract detrended time series
+      df$seasonal_bm_m2 <- as.numeric(fit$time.series[,"seasonal"])
+      # return dataframe
+      return(df)
+}
+
+ss_seasonal <- ss_detrended |> 
+      group_by(estuary, zone) |> 
+      group_split() |> 
+      lapply(seasonality) |> 
+      bind_rows()
+
+### checking out seasonality ---
+ss_seasonal |> 
+      filter(estuary == "Apalachicola Bay", zone == "A", year == 2010) |> 
+      ggplot(aes(x=date, y = seasonal_bm_m2)) +
+      geom_line(size = 1) +
+      labs(title = "Seasonal Component of Biomass",
+           x = "Date",
+           y = "Biomass (m2)") +
+      theme_minimal()
+
+### notes - data really isn't there for some of the sites - especially true with haul seine data
