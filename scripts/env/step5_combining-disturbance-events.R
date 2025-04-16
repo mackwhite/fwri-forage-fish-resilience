1a5b58e863673caa4f9ada5d97ba5bdce6abe329
mackwhite <mwhite@fiu.edu>
2025-04-16 11:53:25 -0400

    safety push

---
+++ scripts/env/step5_combining-disturbance-events.R
@@ -0,0 +1,33 @@
+###project: Forage Fish Resilience
+###author(s): MW
+###goal(s): combining disturbance events
+###date(s): January 2025
+###note(s): 
+
+# Housekeeping ------------------------------------------------------------
+
+### load necessary libraries ---
+# install.packages("librarian")
+librarian::shelf(tidyverse, readxl, dplyr, splitstackshape, purrr, zoo, pracma)
+
+### set simple workflow functions ---
+nacheck <- function(df) {
+      na_count_per_column <- sapply(df, function(x) sum(is.na(x)))
+      print(na_count_per_column)
+}
+
+### set color pallete for visualizations ---
+estuary_palette = c("Apalachicola Bay"="#e6d7b9",
+                    "Charlotte Harbor"="#a7c4a0",
+                    "Cedar Key"="#64a988",
+                    'Northern Indian River'="#89c8d9",
+                    "Northeast Florida"="#f2c6b4",
+                    'Tampa Bay'='#dba5a4',
+                    "Southern Indian River"="#bfaed9")
+
+severity_palette = c("Moderate"="#88C0D0",
+                     "Significant"="#EBCB8B",
+                     "Severe"="#BF616A",
+                     "Extreme"="black")
+
+### read in data and bind -----
