458ce3ca2ad487e160becc1d4e62506e78282acc
mackwhite <mwhite@fiu.edu>
2025-04-17 11:40:33 -0400

    seasonal detrending and combining disturbance event and fish time series data

--- scripts/analyses/seasonal-detrend-timeseries.R
+++ scripts/analyses/seasonal-detrend-timeseries.R
@@ -23,26 +23,54 @@ df <- read_csv("local-data/key-datasets/discrete-community-timeseries.csv")
 
 small_seine_zone_df <- df |> 
       filter(gear_details == "21.3-m Seine") |> 
+      # group_by(year, month, day, estuary, zone) |> 
       group_by(year, month, estuary, zone) |> 
-      summarize(bm_m2 = mean(total_biomass_m2)) |> 
+      summarize(bm_m2 = mean(total_biomass_m2), 
+             abund = mean(total_n),
+             max_size = mean(max_size),
+             k = mean(mean_k),
+             gen_time = mean(mean_generation_time), 
+             max_depth = mean(mean_depth_max), 
+             min_depth = mean(mean_depth_min), 
+             pref_temp_min = mean(mean_temp_pref_min), 
+             pref_temp_mean = mean(mean_tem_pref_mean), 
+             pref_temp_max = mean(mean_temp_pref_max), 
+             trophic_level = mean(mean_troph), 
+             benthic_prop = mean(benthic_n), 
+             pelagic_prop = mean(pelagic_n)) |> 
       ungroup() |> 
-      mutate(date = make_date(year = year, month = month, day = 1)) |> 
+      mutate(simple_date = make_date(year = year, month = month, day = 1)) |> 
       mutate(est_zone = paste(estuary, zone, sep = "_"))
+glimpse(small_seine_zone_df)
 
 big_seine_zone_df <- df |> 
       filter(gear_details == "Haul Seine") |> 
+      # group_by(year, month, day, estuary, zone) |> 
       group_by(year, month, estuary, zone) |> 
-      summarize(bm_m2 = mean(total_biomass_m2)) |> 
+      summarize(bm_m2 = mean(total_biomass_m2), 
+             abund = mean(total_n),
+             max_size = mean(max_size),
+             k = mean(mean_k),
+             gen_time = mean(mean_generation_time), 
+             max_depth = mean(mean_depth_max), 
+             min_depth = mean(mean_depth_min), 
+             pref_temp_min = mean(mean_temp_pref_min), 
+             pref_temp_mean = mean(mean_tem_pref_mean), 
+             pref_temp_max = mean(mean_temp_pref_max), 
+             trophic_level = mean(mean_troph), 
+             benthic_prop = mean(benthic_n), 
+             pelagic_prop = mean(pelagic_n)) |> 
       ungroup() |> 
-      mutate(date = make_date(year = year, month = month, day = 1)) |> 
-      mutate(est_zone = paste(estuary, zone, sep = "_"))
+      mutate(simple_date = make_date(year = year, month = month, day = 1)) |> 
+      mutate(est_zone = paste(estuary, zone, sep = "_")) |> 
+      distinct()
+glimpse(big_seine_zone_df)
 
 ### generate function to pull out trend for each estuary-zone combination ----
-
 detrend <- function(df) {
       # arrange everything by data
       df <- df |> 
-            arrange(date)
+            arrange(simple_date)
       # interpolate missing values
       df$bm_m2 <- na.approx(df$bm_m2, na.rm = FALSE)
       # check if the group has enough data (at least 24 months)
@@ -52,7 +80,7 @@ detrend <- function(df) {
       }
       # convert to tsibble for irregular time series handling
       df_tsibble <- df |> 
-            as_tsibble(index = date)
+            as_tsibble(index = simple_date)
       # convert to time series format
       tsdf <- ts(df_tsibble$bm_m2, frequency = 12, start = c(min(df$year), min(df$month)))
       # fit stl to determine seasonality - 'periodic' means fixed seasonality
@@ -68,7 +96,7 @@ ss_detrended <- small_seine_zone_df |>
       group_split() |> 
       lapply(detrend) |> 
       bind_rows() |> 
-      arrange(est_zone, date) |> 
+      arrange(est_zone, simple_date) |> 
       group_by(est_zone) |> 
       mutate(n = n()) |> 
       ungroup()
@@ -76,7 +104,7 @@ ss_detrended <- small_seine_zone_df |>
 ### checking out how seasonality was handled ---
 ss_detrended |> 
       filter(estuary == "Apalachicola Bay", zone == "A") |> 
-      ggplot(aes(x=date)) +
+      ggplot(aes(x=simple_date)) +
       geom_line(aes(y = bm_m2, color = "Original bm_m2"), size = 1) +
       geom_line(aes(y = detrended_bm_m2, color = "Detrended bm_m2"), size = 1, linetype = "dashed") +
       labs(title = "Comparison of Original and Detrended Biomass",
@@ -87,7 +115,7 @@ ss_detrended |>
 
 ss_detrended |> 
       filter(estuary == "Apalachicola Bay", zone == "C") |> 
-      ggplot(aes(x=date)) +
+      ggplot(aes(x=simple_date)) +
       geom_line(aes(y = bm_m2, color = "Original bm_m2"), size = 1) +
       geom_line(aes(y = detrended_bm_m2, color = "Detrended bm_m2"), size = 1, linetype = "dashed") +
       labs(title = "Comparison of Original and Detrended Biomass",
@@ -97,11 +125,10 @@ ss_detrended |>
       theme_minimal()
 
 ### generate function to visualize seasonality for each estuary-zone combination ----
-
 seasonality <- function(df) {
       # arrange everything by data
       df <- df |> 
-            arrange(date)
+            arrange(simple_date)
       # interpolate missing values
       # df$bm_m2 <- na.approx(df$bm_m2, na.rm = FALSE)
       # check if the group has enough data (at least 24 months)
@@ -131,14 +158,14 @@ ss_seasonal <- ss_detrended |>
 ### checking out seasonality ---
 ss_seasonal |> 
       filter(estuary == "Apalachicola Bay", zone == "A", year == 2010) |> 
-      ggplot(aes(x=date, y = seasonal_bm_m2)) +
+      ggplot(aes(x=simple_date, y = seasonal_bm_m2)) +
       geom_line(size = 1.5, color = "black", linetype = "dotted") +
       geom_smooth(size = 2) +
       labs(title = "Seasonal Component of Biomass",
            x = "Date",
            y = "Biomass (m2)") +
       theme_minimal()
-
-write_csv(ss_seasonal, "local-data/detrended-zone-biomass-ts.csv")
+glimpse(ss_seasonal)
+write_csv(ss_seasonal, "local-data/detrended-smallseine-zone-biomass-ts.csv")
 
 ### notes - data really isn't there for some of the sites - especially true with haul seine data
--- scripts/env/step5_combining-disturbance-events.R
+++ scripts/env/step5_combining-disturbance-events.R
@@ -31,3 +31,102 @@ severity_palette = c("Moderate"="#88C0D0",
                      "Extreme"="black")
 
 ### read in data and bind -----
+heatwaves <- read_csv('local-data/marine-heat-wave-severity.csv')
+glimpse(heatwaves)
+
+coldsnaps <- read_csv('local-data/marine-cold-snap-severity.csv')
+glimpse(coldsnaps)
+
+fish <- read_csv('local-data/detrended-smallseine-zone-biomass-ts.csv')
+glimpse(fish)
+
+### select columns to move forward
+mhw <- heatwaves |> 
+      select(year, month, estuary, event, mhw_event_id, mhw_start, mhw_end, 
+             duration, max_anomaly, mean_anomaly, variance_anomaly, cum_anomaly,
+             flag) |> 
+      mutate(dist_id = paste(event, mhw_event_id, sep = "_")) |> 
+      select(-mhw_event_id) |> 
+      rename(event_start = mhw_start,
+             event_end = mhw_end,
+             abs_anomaly = max_anomaly)
+glimpse(mhw)
+
+mcs <- coldsnaps |> 
+      select(year, month, estuary, event, coldsnap_event_id, coldsnap_start, coldsnap_end, 
+             duration, min_anomaly, mean_anomaly, variance_anomaly, cum_anomaly,
+             flag) |> 
+      mutate(dist_id = paste(event, coldsnap_event_id, sep = "_")) |> 
+      select(-coldsnap_event_id) |> 
+      rename(event_start = coldsnap_start,
+             event_end = coldsnap_end,
+             abs_anomaly = min_anomaly)      
+glimpse(mcs)
+
+dist <- rbind(mhw,mcs) |> distinct()
+
+fullj <- full_join(fish, dist)
+glimpse(fullj)
+
+### clean up envi ---
+### clean environment ---
+keep <- c("fullj")
+rm(list = setdiff(ls(), keep))
+glimpse(fullj)
+
+all <- fullj |> 
+      select(estuary, zone, year, month, simple_date, 
+             detrended_bm_m2, max_size, gen_time, max_depth, min_depth,
+             pref_temp_min, pref_temp_mean, pref_temp_max, trophic_level, 
+             benthic_prop, pelagic_prop,
+             event, dist_id, event_start, event_end, duration,
+             abs_anomaly, mean_anomaly, variance_anomaly, cum_anomaly, 
+             flag) |> 
+      arrange(estuary, zone, year, month)
+glimpse(all)
+
+all1 <- all |> 
+      mutate(treatment = case_when(
+            !is.na(dist_id) ~ "dist",
+            !is.na(lead(dist_id)) & is.na(dist_id) ~ "cont",
+            TRUE ~ "other"
+      )) |> 
+      mutate(dist_id = case_when(
+            !is.na(lead(dist_id)) & is.na(dist_id) ~ lead(dist_id),
+            TRUE ~ dist_id
+      )) |> 
+      mutate(event = case_when(
+            !is.na(lead(event)) & is.na(event) ~ lead(event),
+            TRUE ~ event
+      )) |> 
+      filter(treatment != "other") |> 
+      arrange(estuary, zone, year, month)
+glimpse(all1)
+
+all2 <- all1 |> 
+      pivot_wider(names_from = treatment, 
+                  values_from = c(
+                        detrended_bm_m2,
+                        max_size,
+                        gen_time,
+                        max_depth, 
+                        min_depth,
+                        pref_temp_min,
+                        pref_temp_mean,
+                        pref_temp_max,
+                        trophic_level,
+                        benthic_prop,
+                        pelagic_prop), 
+                  names_glue = "{treatment}_{.value}") |> 
+      select(-c(cont_benthic_prop, dist_benthic_prop, cont_pelagic_prop, dist_pelagic_prop)) |> 
+      arrange(estuary, zone, year, month) |> 
+      group_by(estuary, zone) |> 
+      fill(starts_with("cont_"), .direction = "down") |> 
+      ungroup() |> 
+      select(-c(event_start, event_end, simple_date)) |> 
+      filter(!is.na(duration),
+             !is.na(zone))
+glimpse(all2)
+
+all3 <- all2 |> 
+      mutate(resistance = log(dist_detrended_bm_m2/cont_detrended_bm_m2))
